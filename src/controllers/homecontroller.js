e8cc370b0c060b698cf71a5a6a901ecd40e8005f
hoanghac <khongcono92@example.com>
2022-05-07 14:04:31 +0700

    connecdb, render data to view

--- src/controllers/homecontroller.js
+++ src/controllers/homecontroller.js
@@ -1,6 +1,16 @@
 
-let getHomePage = (req, res) => {
-    return res.render('home.ejs');
+import db from '../models/index';
+
+let getHomePage = async (req, res) => {
+
+    try{
+        let data = await db.User.findAll();     
+        return res.render('home.ejs', {
+            data : JSON.stringify(data)
+        });
+    }catch(e){
+        console.log(e);
+    }
 }
 
 let getAboutPage = (req, res) => {
